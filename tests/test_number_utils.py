6d0646ecee13b4b3e7d85ea2383cc007210d087f
Suttirak Mattayawerakiat <raksuttirak2@gmail.com>
2022-08-02 11:24:11 +0700

    add 5 test cases

---
+++ tests/test_number_utils.py
@@ -0,0 +1,35 @@
+import unittest
+from coe_number.number_utils import is_prime_list
+
+
+class PrimeListTest(unittest.TestCase):
+    def test_give_2_3_is_prime(self):
+        prime_list = [2, 3]
+        is_prime = is_prime_list(prime_list)
+        self.assertTrue(is_prime)
+
+    def test_give_4_6_10_is_not_prime(self):
+        prime_list = [4,6,10]
+        is_prime = is_prime_list(prime_list)
+        self.assertFalse(is_prime)
+
+    def test_give_9_15_21_is_not_prime(self):
+        prime_list = [9,15,21]
+        is_prime = is_prime_list(prime_list)
+        self.assertFalse(is_prime)
+
+    def test_give_0_1_is_not_prime(self):
+        prime_list = [0,1]
+        is_prime = is_prime_list(prime_list)
+        self.assertFalse(is_prime)
+
+    def test_give_neg_2_3_5_7_is_prime(self):
+        prime_list = [-2,-3,-5,-7]
+        is_prime = is_prime_list(prime_list)
+        self.assertTrue(is_prime)
+
+    def test_give_neg_4_1_9_15_is_not_prime(self):
+        prime_list = [-4,-1,-9,-15]
+        is_prime = is_prime_list(prime_list)
+        self.assertFalse(is_prime)
+
